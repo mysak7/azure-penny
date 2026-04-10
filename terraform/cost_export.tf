41edf236f20d2217dd31c838cc80b7ba3922fca1
mysak7 <michal.burdik@proton.me>
2026-04-10 19:59:08 +0000

    fix: switch cost export format from CSV to Parquet

--- terraform/cost_export.tf
+++ terraform/cost_export.tf
@@ -13,7 +13,7 @@ resource "azurerm_subscription_cost_management_export" "daily" {
   recurrence_period_start_date = "2026-04-10T00:00:00Z"
   recurrence_period_end_date   = "2099-12-31T00:00:00Z"
 
-  file_format = "Csv"
+  file_format = "Parquet"
 
   export_data_storage_location {
     container_id     = "${azurerm_storage_account.main.id}/blobServices/default/containers/${azurerm_storage_container.cost_exports.name}"
