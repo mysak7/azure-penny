81d82965735d50d9d7b4973ce78410eee7791a14
mysak7 <michal.burdik@proton.me>
2026-04-10 20:16:17 +0000

    fix: import and pin acr_pull role assignment name to prevent 409 conflicts

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -10,6 +10,7 @@ resource "azurerm_container_registry" "main" {
 
 # Allow the Container App's managed identity to pull images from ACR.
 resource "azurerm_role_assignment" "acr_pull" {
+  name                 = "ee571f4c-2573-1f4e-47d7-84eebffd5be5"
   scope                = azurerm_container_registry.main.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_user_assigned_identity.main.principal_id
